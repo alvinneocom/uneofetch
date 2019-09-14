821c5162b5ffe6f5f156487688e5f73673b41313
Alvin Rhaman <50039002+alvinneocom@users.noreply.github.com>
2019-09-14 03:40:00 -0500

    final update to install.sh
    rip install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 # this script installs uneofetch
 
+cd ~
+mkdir uneofetch-installation
+git clone https://github.com/alvinneocom/uneofetch.git
+cd uneofetch
 cp ./uneofetch.sh /bin/uneofetch
 cd /bin
 chmod +x uneofetch
+cd ~
+echo "Your installation is complete. Please delete the directory uneofetch-installation.\n"
+echo "uneofetch is still in early stages. Please report any bugs in Issues."
