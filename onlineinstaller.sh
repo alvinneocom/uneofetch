97603f13cbe4a2b715dd490d203b6e05aca9e6dc
Alvin Rhaman <50039002+alvinneocom@users.noreply.github.com>
2019-09-14 21:37:58 -0500

    Update onlineinstaller.sh

--- onlineinstaller.sh
+++ onlineinstaller.sh
@@ -1,24 +1,16 @@
 # Run this to install on Ubuntu/Debian-based distros.
 # Some components of uneofetch don't work on other distros. Keep this in mind.
 # This script installs uneofetch directly from the Github.
-
-cd ~
-folder=./uneofetch-installation
-if test -d $folder; then
-rm -r uneofetch-installation
-fi
-mkdir uneofetch-installation
-cd ./uneofetch-installation
+# Installer by pepsipu.
 git clone https://github.com/alvinneocom/uneofetch.git
 cd uneofetch
-installation=usr/bin/uneofetch
-if test -f $installation; then
+if test -f /usr/bin/uneofetch; then
 rm /usr/bin/uneofetch
 fi
 chmod +x ./uneofetch.sh
 cp ./uneofetch.sh /usr/bin/uneofetch
-rm -r ~/uneofetch-installation
-cd ~
+cd ..
+rm -r uneofetch
 echo "Your installation is complete.\n"
 echo "uneofetch is still in early stages. Please report any bugs in Issues.\n"
 echo "Notes: uneofetch is based off of ufetch Ubuntu. Some functions will fail to work in other distros.\n"
