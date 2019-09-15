017825ae1ed0efadc286ada1d7d9987fb85d401d
Alvin Rhaman <50039002+alvinneocom@users.noreply.github.com>
2019-09-14 21:32:58 -0500

    fixed everything (i hope)

--- onlineinstaller.sh
+++ onlineinstaller.sh
@@ -5,24 +5,21 @@
 cd ~
 folder=./uneofetch-installation
 if test -d $folder; then
-echo "Please delete the uneofetch-installation directory. After this install, delete the uneofetch-installation-1 directory."
-mkdir uneofetch-installation-1
-cd ./uneofetch-installation-1
-else
+rm -r uneofetch-installation
+fi
 mkdir uneofetch-installation
 cd ./uneofetch-installation
-fi
 git clone https://github.com/alvinneocom/uneofetch.git
 cd uneofetch
 installation=usr/bin/uneofetch
 if test -f $installation; then
 rm /usr/bin/uneofetch
 fi
+chmod +x ./uneofetch.sh
 cp ./uneofetch.sh /usr/bin/uneofetch
-cd /bin
-chmod +x uneofetch
+rm -r ~/uneofetch-installation
 cd ~
-echo "Your installation is complete. Please delete the directory uneofetch-installation.\n"
+echo "Your installation is complete.\n"
 echo "uneofetch is still in early stages. Please report any bugs in Issues.\n"
 echo "Notes: uneofetch is based off of ufetch Ubuntu. Some functions will fail to work in other distros.\n"
 echo "The path directory may not be bin all the time."
